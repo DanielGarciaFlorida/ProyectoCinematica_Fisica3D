bc8cc53040309e0d3e37a72519e61d1320b8a318
DanielGarcia <dagana@alumnatflorida.es>
2026-05-05 11:28:45 +0200

    Update BallController, correción de fisicas segun tiro parabolico

--- Assets/ProyectoCinematica/Scripts/BallController.cs
+++ Assets/ProyectoCinematica/Scripts/BallController.cs
@@ -3,27 +3,57 @@ using UnityEngine;
 public class BallController : MonoBehaviour
 {
     Rigidbody rb;
+    private bool isLaunched = false;
+
+    //Formula tiro parabolico.
+    //1.En eje X: x = v0 * cos(alpha) * t.
+    //2.En eje Y: y = y0 + v0 * sin(alpha) * t - (0.5 * g * t^2)
+
+    private Vector3 initialVelocity;
+    private Vector3 startPosition;
+    private float launchTime;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        if (!isLaunched) return;
+
+
+       
     }
 
     public void Launch(Vector3 velocity)
     {
         rb.isKinematic = false;
-        rb.linearVelocity = velocity;
+
+        initialVelocity = velocity;
+        startPosition = transform.position;
+        launchTime = Time.time;
+
+        isLaunched = true;
     }
+
+    Vector3 CalculatePosition(float time)
+    {
+        float gravity = Physics.gravity.y;
+
+        float x = startPosition.x + initialVelocity.x * time;
+        float y = startPosition.y + initialVelocity.y * time + 0.5f * gravity * time * time;
+        float z = startPosition.z + initialVelocity.z * time;
+
+        return new Vector3(x, y, z);
+    }
+
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Target"))
+        if (collision.gameObject.CompareTag("Hole"))
         {
-            Debug.Log("HIT");
+            Debug.Log("Goal);
             //llamar al GameManager
         }
-        else if (collision.gameObject.CompareTag("Ground"))
-        {
-            Debug.Log("MISS");
-            //llamar al GameManager
-        }
+        
     }
 }
