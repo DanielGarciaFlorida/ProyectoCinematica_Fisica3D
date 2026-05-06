dcdcb3765538ea8bcd19ccc8c364c0f96f8d93b5
ismcga <ismcga@alumnatflorida.es>
2026-05-07 00:08:22 +0200

    Errores de UI y  sistema de inicio arreglados

--- Assets/ProyectoCinematica/Scripts/DestructibleObject.cs
+++ Assets/ProyectoCinematica/Scripts/DestructibleObject.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 
 public class DestructibleObject : MonoBehaviour
 {
+   public GameObject canvas;
+
+    private void Awake()
+    {
+        canvas.SetActive(false);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Ball"))
         {
-            UIManager.Instance.GameOver();
+           canvas.SetActive(true);
         }
     }
 }
--- Assets/ProyectoCinematica/Scripts/UIManager.cs
+++ Assets/ProyectoCinematica/Scripts/UIManager.cs
@@ -6,23 +6,11 @@ public class UIManager : MonoBehaviour
 
     public GameObject gameOverPanel;
 
-    private void Awake()
-    {
-        Instance = this;
-
-        // 🔴 IMPORTANTE: ocultar al empezar
-        gameOverPanel.SetActive(false);
-    }
-
-    public void GameOver()
-    {
-        gameOverPanel.SetActive(true);
-        Time.timeScale = 0f;
-    }
+ 
 
     public void RestartButton()
     {
-        Time.timeScale = 1f;
+       
         GameManager.Instance.RestartLevel();
     }
 }
